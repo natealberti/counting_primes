1b6196a3db4392715a90cd79b3c593e32046cc8d
fire774 <65456795+natealberti@users.noreply.github.com>
2022-02-05 23:31:01 -0700

    maven

---
+++ src/main/java/CountingPrimes.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class CountingPrimes {
+
+    public static void main(String []args){
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        // parsing stdin into ints
+        String[] arr = input.split(",");
+        int n = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+
+        // finding the primes between m and n (inclusive)
+        int accumulator = 0;
+        for(int i = n; i < m; i++) {
+            if(prime(i))
+                accumulator++;
+        }
+
+        System.out.println(accumulator);
+
+    }
+
+    // tests if a number is prime
+    public static boolean prime(int num) {
+        for(int i=2;i<=num/2;i++) {
+            int temp = num%i;
+            if(temp==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
